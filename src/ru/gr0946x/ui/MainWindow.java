b80b028c1bb2b716da4d70343ff4cdba455b6066
Smak2025 <smak-80+2025@yandex.ru>
2026-03-18 18:55:31 +0300

    - Добалвение основной панели

--- src/ru/gr0946x/ui/MainWindow.java
+++ src/ru/gr0946x/ui/MainWindow.java
@@ -1,4 +1,26 @@
 package ru.gr0946x.ui;
 
-public class MainWindow {
+import javax.swing.*;
+import java.awt.*;
+
+public class MainWindow extends JFrame {
+
+    private JPanel mainPanel = new JPanel();
+    public MainWindow(){
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setMinimumSize(new Dimension(500, 450));
+        mainPanel.setBackground(Color.WHITE);
+        var gl = new GroupLayout(getContentPane());
+        setLayout(gl);
+        gl.setVerticalGroup(gl.createSequentialGroup()
+                .addGap(8)
+                .addComponent(mainPanel, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE)
+                .addGap(8)
+        );
+        gl.setHorizontalGroup(gl.createSequentialGroup()
+                .addGap(8)
+                .addComponent(mainPanel, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE)
+                .addGap(8)
+        );
+    }
 }
